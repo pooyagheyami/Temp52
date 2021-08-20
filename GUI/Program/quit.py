f2a4600950892dea9ab977b9a3bb712c1d5c0d73
pooya <pooyagheyami@gmail.com>
2021-08-20 19:29:31 +0300

    Update it 20-08

---
+++ GUI/Program/quit.py
@@ -0,0 +1,24 @@
+#In the name of GOD
+#please put your code under here
+
+import wx
+
+def size():
+    return (-1,-1)
+
+def main(panel=None):
+    #print 'out'
+    q= wx.GetActiveWindow()
+    p= wx.GetApp()
+    
+    if str(q) == 'None':
+        p.Exit()
+        wx.App_CleanUp()
+    else:
+        #print q.m_mgr
+        q.m_mgr.UnInit()
+        q.Destroy()
+        wx.App_CleanUp()
+    
+if __name__ == '__main__':
+    main(None)
